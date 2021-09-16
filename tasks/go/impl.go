9d8443662c4a7287a5e94bd4b8c87668675675bf
Artem Streltsov <35077603+kekekekule@users.noreply.github.com>
2021-09-16 21:03:08 +0300

    Update impl.go

--- tasks/go/impl.go
+++ tasks/go/impl.go
@@ -1,19 +1,123 @@
 package ordcol
 
+import (
+	"sort"
+)
+
 type item struct {
-    // your code here
+    key int
+	value int
 }
 
 type collection struct {
-    // your code here
+	storage map[int]Item
+	currentOrder int
+	orderStorage map[int]Item
+	keys map[int]bool
+}
+
+type collectionIterator struct {
+	index int
+	keys []int
+	collection *map[int]Item
 }
 
 func NewItem(key int, value int) *item {
-    // your code here
+    return &item{
+		key: key,
+		value: value,
+	}
 }
 
 func NewCollection() *collection {
-    // your code here
+	return &collection{
+		storage: make(map[int]Item),
+		orderStorage: make(map[int]Item),
+		currentOrder: 0,
+		keys: make(map[int]bool),
+	}
 }
 
-// your code here
+func (item item) Key() int {
+	return item.key
+}
+
+func (item item) Value() int {
+	return item.value
+}
+
+func (iter *collectionIterator) HasNext() bool {
+	if iter.index < len(iter.keys) {
+		return true
+	}
+
+	return false
+}
+
+func (iter *collectionIterator) Next() (Item, error) {
+	if iter.HasNext() {
+		itm := (*iter.collection)[iter.keys[iter.index]]
+
+		iter.index++
+
+		return itm, nil
+	}
+
+	return nil, ErrEmptyIterator
+}
+
+func (collection *collection) At(key int) (Item, bool) {
+	if collection.keys[key] {
+		return collection.storage[key], true
+	}
+	return nil, false
+}
+
+func (collection *collection) Add(item Item) error {
+	key := item.Key()
+	if collection.keys[key] {
+		return ErrDuplicateKey
+	}
+
+	collection.keys[key] = true
+	collection.storage[key] = item
+	collection.orderStorage[collection.currentOrder] = item
+	collection.currentOrder++
+
+	return nil
+}
+
+func (collection *collection) IterateBy (order IterationOrder) Iterator {
+	mp := collection.storage
+	if order == ByInsertion {
+		keys := make([]int, len(mp))
+
+		for i := 0; i < len(mp); i++ {
+			keys[i] = i
+		}
+
+		return &collectionIterator{
+			index: 0,
+			keys: keys,
+			collection: &collection.orderStorage,
+		}
+
+	} else if order == ByKey {
+		keys := make([]int, len(mp))
+
+		i := 0
+		for k := range mp {
+			keys[i] = k
+			i++
+		}
+
+		sort.Ints(keys)
+		return &collectionIterator{
+			index: 0,
+			keys: keys,
+			collection: &collection.storage,
+		}
+	} else {
+		panic("no such order")
+	}
+}
